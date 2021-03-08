c0d4af8443cd183601137217b333a044e039be73
Peeraphat <peeraphat@frappet.com>
2021-03-08 10:48:58 +0700

    แก้ไข Text login

--- src/views/pages/Login.js
+++ src/views/pages/Login.js
@@ -109,7 +109,7 @@ const Login = () => {
                     <form onSubmit={submitLogin}>
                       <div className="form-group mb-3">
                         <CLabel style={{ color: "#7e7e7e" }}>
-                          ชื่อผู้ใช้งานasdasd
+                          ชื่อผู้ใช้งาน
                         </CLabel>
                         <CInputGroup>
                           <CInputGroupPrepend>
